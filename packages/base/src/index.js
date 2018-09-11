8c64bf4dcf6d7c00a240d0d53f2b23daebcec210
orlo wang <ow.cc@outlook.com>
2018-09-11 13:45:20 +0800

    fix miss endpoint

---
+++ packages/base/src/index.js
@@ -0,0 +1,4 @@
+export { default as Flex } from "./flex";
+export { default as Text } from "./text";
+export { default as Avatar } from "./avatar";
+export { default as AvatarCard } from "./avatarCard";
